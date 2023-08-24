13abf235b189e9bb1a8ec78ac3b6dc2c0021c43f
Daria Evelyna Bujor <evelynadaria3@gmail.com>
2023-08-24 17:54:53 +0300

    intake toggle

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOp.java
@@ -4,36 +4,46 @@ import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
+import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
+
 import java.util.List;
 
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "armata moldovei")
 public class TeleOp extends OpMode {
-//    private Drivetrain drive;
+    private Drivetrain drive;
     private DcMotorEx intake;
 
-
+    boolean currentA;
+    boolean lastA;
+    boolean intakeToggle;
 
     @Override
     public void init() {
-//        drive = new Drivetrain(hardwareMap);
+        drive = new Drivetrain(hardwareMap);
         intake = hardwareMap.get(DcMotorEx.class, "intake");
         List<LynxModule> allHubs = hardwareMap.getAll(LynxModule.class);
-        for(LynxModule hub : allHubs) {
+        for (LynxModule hub : allHubs) {
             hub.setBulkCachingMode(LynxModule.BulkCachingMode.AUTO);
         }
-
-
     }
 
-
+    public void intake() {
+        lastA = currentA;
+        currentA = gamepad1.a;
+        if (currentA) {
+            intakeToggle = !intakeToggle;
+            if (intakeToggle) {
+                intake.setPower(0);
+            } else {
+                intake.setPower(1);
+            }
+        }
+    }
 
 
     @Override
     public void loop() {
 //        drive.update(gamepad1);
-        if(gamepad1.a)
-            intake.setPower(1);
-        else
-            intake.setPower(0);
+        intake();
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
@@ -19,6 +19,7 @@ public class Drivetrain {
     DcMotor motorBackLeft;
     DcMotor motorFrontRight;
     DcMotor motorBackRight;
+
     IMU imu;
 
 
@@ -44,18 +45,18 @@ public class Drivetrain {
     }
 
     public void update(Gamepad gamepad1) {
-        double y = -gamepad1.left_stick_y; // Remember, this is reversed!
-        double x = -gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
-        double rx = gamepad1.right_stick_x;
+        double drive = -gamepad1.left_stick_y; // Remember, this is reversed!
+        double strafe = -gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+        double turn = gamepad1.right_stick_x;
 
         // Denominator is the largest motor power (absolute value) or 1
         // This ensures all the powers maintain the same ratio, but only when
         // at least one is out of the range [-1, 1]
-        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-        double frontLeftPower = (y + x + rx) / denominator;
-        double backLeftPower = (y - x + rx) / denominator;
-        double frontRightPower = (y - x - rx) / denominator;
-        double backRightPower = (y + x - rx) / denominator;
+        double denominator = Math.max(Math.abs(drive) + Math.abs(strafe) + Math.abs(turn), 1);
+        double frontLeftPower = (drive + strafe + turn) / denominator;
+        double backLeftPower = (drive - strafe + turn) / denominator;
+        double frontRightPower = (drive - strafe - turn ) / denominator;
+        double backRightPower = (drive + strafe - turn) / denominator;
 
 
         if(gamepad1.left_bumper) {
